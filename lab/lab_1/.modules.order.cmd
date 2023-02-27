7c6a911ac1ebd0d0492c44c43f68e1e3c78ac13f
Nikita-ctr <xmil.nik@gmail.com>
2023-02-27 14:57:55 +0300

    gi

--- lab/lab_1/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/nikita/APK/lab_1/modules.order := {   echo /home/nikita/APK/lab_1/readport.ko; :; } | awk '!x[$$0]++' - > /home/nikita/APK/lab_1/modules.order
--- lab/lab_1/.readport.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/nikita/APK/lab_1/readport.mod := { echo  /home/nikita/APK/lab_1/readport.o;  echo; } > /home/nikita/APK/lab_1/readport.mod
